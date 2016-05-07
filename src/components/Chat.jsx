449e00a7a61dd08f278ad9b63faeee38abfacad3
Elior <elior.b@dynamicyield.com>
2016-05-07 11:47:28 +0300

    redirect login

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import {withRouter} from 'react-router';
 
 import MessageList from './message/MessageList.jsx';
 import ChannelList from './channel/ChannelList.jsx';
 import MessageBox from './message/MessageBox.jsx';
+
+import ChatStore from '../stores/ChatStore';
 
 class Chat extends React.Component {
   render () {
@@ -20,6 +22,16 @@ class Chat extends React.Component {
       </div>
     );
   }
+
+  /**
+   * Called before transitioning
+   */
+  componentWillMount () {
+    var state = ChatStore.getState();
+    if (!state.user.uid) {
+      this.props.router.replace('/login');
+    }
+  }
 }
 
-export default Chat;
+export default withRouter(Chat);
